2677d9ecd7a19531c6de3803dbd3d31da237fd37
al-dr1n <97592969+al-dr1n@users.noreply.github.com>
2022-02-12 12:17:19 +0800

    Create Hangman_words.py
    Hangman_words.py are run by Hangman_play.py --- aldrin_viado

---
+++ Hangman_words.py
@@ -0,0 +1,20 @@
+""" David, I am having a hard time combining this with your code.
+    Hope you can figure it out how to merge our codes together.
+                                               ---aldrin_viado"""
+
+import random
+
+class RandomWord:
+
+    def __init__(self):
+        pass
+
+def choices():
+ 
+    word_list = ["python", "program", "pathway", "hearing","bayou", "regimen", "drain", "bereft", "matte", "bill","medal", "prickly", "sarcasm", "stuffy", "alleged","monopoly", "lighter", "repair", "worship", "vent","hybrid", "buffet"]
+ 
+    word = random.choice(word_list)
+ 
+    return word
+
+
